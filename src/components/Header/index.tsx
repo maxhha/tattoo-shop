d6b70e16106c06254c7141d0936127d4c38d4a08
maxhha <devmaxhha@gmail.com>
2019-08-10 22:48:21 +0300

    Add sections menu popup

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,37 @@
 // Header
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LinkButton from 'components/commons/LinkButton';
 import classNames from "classnames";
 import logo from "assets/images/logo.png";
 import "./Header.scss";
 
+import { StoreSection } from "types";
+import StoreContext from "apis/StoreContext";
+
 type Props = {className?: string}
 
 const Header : React.SFC<Props> = (props) => {
+
+    const  [sections, setSections] = React.useState([] as StoreSection[]);
+    const { getSections } = React.useContext(StoreContext);
+    React.useEffect(() => {
+        getSections().then((s) => setSections(s))
+    }, [getSections])
+
     return (
         <header className={classNames("header", props.className)}>
             <nav className="nav header__side header__nav">
-                <LinkButton to='/' className="button nav__button">Категроии</LinkButton>
+                <div className="menu nav__menu">
+                    <div className="menu__title">Категроии</div>
+                    <ul className="menu__popup-list">
+                        {sections.map(({title}) =>
+                            <li className="menu__item">
+                                <Link to="/" className="nav__menu-item" >{title}</Link>
+                            </li>
+                        )}
+                    </ul>
+                </div>
                 <LinkButton to='/about' className="button nav__button">О нас</LinkButton>
                 <button className="button nav__button">Контакты</button>
             </nav>
