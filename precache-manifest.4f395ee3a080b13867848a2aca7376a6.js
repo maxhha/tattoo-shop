b4bc2599e095e8332929c7b1f3923363a9954e98
maxhha <devmaxhha@gmail.com>
2019-08-18 11:35:19 +0300

    Updates

---
+++ precache-manifest.4f395ee3a080b13867848a2aca7376a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f172e292556be39ba173480eb98a9c",
+    "url": "/tattoo-shop/index.html"
+  },
+  {
+    "revision": "5f97890a5e4a275a5141",
+    "url": "/tattoo-shop/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d5222b459b8089c68863",
+    "url": "/tattoo-shop/static/css/main.8a892509.chunk.css"
+  },
+  {
+    "revision": "5f97890a5e4a275a5141",
+    "url": "/tattoo-shop/static/js/2.aada7dd3.chunk.js"
+  },
+  {
+    "revision": "d5222b459b8089c68863",
+    "url": "/tattoo-shop/static/js/main.83e81662.chunk.js"
+  },
+  {
+    "revision": "d6a9fa27b963cd5cdbca",
+    "url": "/tattoo-shop/static/js/runtime~main.50c9aa78.js"
+  },
+  {
+    "revision": "2ff07bb3992da0cf65b71b4a1ef2068b",
+    "url": "/tattoo-shop/static/media/face.2ff07bb3.jpg"
+  },
+  {
+    "revision": "99ea3e584bfb39dd34874a1f9be3c9eb",
+    "url": "/tattoo-shop/static/media/logo.99ea3e58.png"
+  }
+]);
